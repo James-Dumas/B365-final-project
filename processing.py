5ec69889952f0a1058a420a6e36727dba2ac9a88
rkellems <70907079+rkellems@users.noreply.github.com>
2021-11-29 14:19:58 -0500

    changed attributes in processing file

--- processing.py
+++ processing.py
@@ -1,4 +1,4 @@
-import json, pandas as pd, numpy as np
+import json, pandas as pd, numpy as np, statistics as stat
 from datetime import datetime
 
 def timeNormal(st):
@@ -23,19 +23,28 @@ def timeNormal(st):
     return datetime(2021,11,int(st[0:2]),hour,minute,sec)
 
 def createRow(f,i):
+    finalPrice = f[i]['bids'][len(f[i]['bids'])-1]['price']
     bidders = f[i]['bidders']
     duration = f[i]['duration']
-    startPrice = f[i]['start_price']
-    startTime = timeNormal(f[i]['start_time'])
-    sellPrice = f[i]['bids'][len(f[i]['bids'])-1]['price']
-    finalBidTime = timeNormal(f[i]['bids'][len(f[i]['bids'])-1]['time'])
-    return np.array([bidders,duration,startPrice,startTime,sellPrice,finalBidTime])
+    prices, times = [], []
+    prices += [f[i]['start_price']]
+    times += [timeNormal(f[i]['start_time'])]
+    for j in range(len(f[i]['bids'])):
+        prices += [f[i]['bids'][j]['price']]
+        times += [timeNormal(f[i]['bids'][j]['time'])]
+    priceDif = np.diff(prices)
+    timeDif = [x.total_seconds() for x in np.diff(times)]
+    priceDifMean = stat.mean(priceDif)
+    priceDifSd = stat.stdev(priceDif) if len(priceDif) > 1 else 0
+    timeDifMean = stat.mean(timeDif)
+    timeDifSd = stat.stdev(timeDif) if len(timeDif) > 1 else 0
+    return np.array([[finalPrice,bidders,duration,priceDifMean,priceDifSd,timeDifMean,timeDifSd]])
 
 f = open('data/scraped/out_2021-11-28_03-28-13.json')
 sales = json.load(f)
 
 data = createRow(sales,0)
-for i in range(1,len(sales)):
+for i in range(1,len(sales)): 
     data = np.vstack([data,createRow(sales,i)])
-df = pd.DataFrame(data,columns=['Bidders','Duration','Start Price','Start Time','Sell Price','Final Bid Time'])
+df = pd.DataFrame(data,columns=['Final Price','Bidders','Duration','Price Dif Mean','Price Dif SD','Time Dif Mean','Time Dif SD'])
 print(df)
