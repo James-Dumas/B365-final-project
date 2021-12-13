0d6433dd1ebe599e5015eae4e82052f18b0588e1
EnoraRec <95255986+EnoraRec@users.noreply.github.com>
2021-12-13 18:03:01 -0500

    Added Multiple Linear Regression
    Also cleaned the code.

--- R regression.R
+++ R regression.R
@@ -1,39 +1,59 @@
 
 
-dat = read.csv(file.choose(),stringsAsFactors=FALSE, sep=",")
-class(dat)
-View(dat)
+data = read.csv(file.choose(),stringsAsFactors=FALSE, sep=",")
+class(data)
+View(data)
 
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-library(Metrics)
 library(FactoMineR)
 
 # delete useless 1st column (indexes)
-df = dat[-1]
-attach(df)
 
+#################################################
+# linear regression with one variable at a time #
+#################################################
 
+sses = rep(0,6)
+As = rep(0,6)
 
-# regression with 1 to 6 attributes
-#################################
-n = nrow(X);
+for (i in c(2:7)) {
+    X = as.matrix(df[,i])
+    print(length(X)==length(Y))
+    Y = as.matrix(Final.Price)
+    n = length(Y)
+    a = solve(t(X) %*% X, t(X) %*% Y)
+    As[i-1] = a
 
-Y = as.matrix(Final.Price)
-for (d in 1:6) {
-  X = as.matrix(df[,2:(2+d-1)]);  
-  a = solve(t(X) %*% X, t(X) %*% Y)  
-  yhat = X %*% a	 
-  error = Y - yhat
-  sse = sum(error*error)
-  cat("with ", d, " variables  got sse of ", sse, "\n");
+    Yhat = X %*% a
+    error = Y - Yhat
+    sse = sum(error**2)
+    sses[i-1] = sse
+
+    xbar = sum(X)/n
+    ybar = sum(Y)/n
+    xybar = sum(X*Y)/n
+    xsqbar = sum(X*X)/n
+
+    b = (ybar*xsqbar-xbar*xybar)/ (xsqbar - xbar*xbar) 
+    print(b)
+    a = (ybar - b)/xbar
+    print(a)
+
+    newplot = ggplot(df, aes(x=df[,i], y=Final.Price)) + 
+        geom_point() +
+        stat_smooth(method='lm', formula = y ~ x, size = 1) + 
+        xlab(names[i]) +
+        ylab('Final Price')
+    show(newplot)
 }
 
+sses
+names[which.min(sses)+1]
 
 
-
-# regression with every attribute
+# Linear regression with every attribute
 #################################
 X = as.matrix(df[,2:7])
 names = colnames(df)
@@ -48,100 +68,62 @@ sse = sum(error**2)
 print(sse)
 
 
+##############################
+# Multiple Linear Regression #
+##############################
+
+# Pair plots
+############
+pairs(df, pch = 18, col = "steelblue")
 
 
-# regression with 1 attribute at a time 
-#######################################
+# Multiple regression
+#####################
 
-sses = rep(0,6)
-As = rep(0,6)
-
-for (i in c(2:7)) {
-  
-
-X = as.matrix(df[,i])
-print(length(X)==length(Y))
+names = colnames(df)
 Y = as.matrix(Final.Price)
-n = length(Y)
-a = solve(t(X) %*% X, t(X) %*% Y)
-As[i-1] = a
 
-Yhat = X %*% a
-error = Y - Yhat
-sse = sum(error**2)
-sses[i-1] = sse
-
-xbar = sum(X)/n
-ybar = sum(Y)/n
-xybar = sum(X*Y)/n
-xsqbar = sum(X*X)/n
-
-b = (ybar*xsqbar-xbar*xybar)/ (xsqbar - xbar*xbar) 
-print(b)
-a = (ybar - b)/xbar
-print(a)
-
-newplot = ggplot(df, aes(x=df[,i], y=Final.Price)) + 
-  geom_point() +
-  stat_smooth(method='lm', formula = y ~ x, size = 1) + 
-  xlab(names[i]) +
-  ylab('Final Price')
-show(newplot)
+for (d in rep(2:6)){
+  for (i in rep(1:6)) {
+    if (d+i < 8){
+      form = DF2formula(df[,d:(d+i)])
+      fit = lm(form, data = df)
+      cat("For the variables",names[d:(d+i)] ,"adjusted R squared is",summary(fit)$adj.r.square, "\n")
+      # consistent variance?
+      plot(fitted(fit), residuals(fit))
+      abline(h = 0, lty = 2)
+      title(names[d:(d+i)])
+      
+    }
+  }
 }
 
-sses
-names[which.min(sses)+1]
 
-
-
-# Principal Component Analysis - python implementation is more complete
-############################## 
-
-dff = df[1:7]
-View(dff)
-
-#pca = PCA(df, scale.unit=TRUE, ncp=2, graph=T)
-pca=PCA(dff, quanti.sup=1)
-plot(pca,habillage= "Final.Price",select="cos2 0.6")
-
-
-
-#############
-# polynomial#  prices won't decrease so doesn't really make sense ¯\_(ツ)_/¯
-#############
-
+#########################
+# Polynomial Regression #
+#########################
 
 df.shuffled <- df[sample(nrow(df[,1:7])),]
 
 K <- 10 
-
-#define degree of polynomials to fit
 degree <- 5
-
-#create k equal-sized folds
 folds <- cut(seq(1,nrow(df.shuffled)),breaks=K,labels=FALSE)
-
-#create object to hold MSE's of models
+# mean square error
 mse = matrix(data=NA,nrow=K,ncol=degree)
 
-#Perform K-fold cross validation
+# K-fold cross validation
 for(i in 1:K){
-  
-  #training and testing data
   testIndexes <- which(folds==i,arr.ind=TRUE)
   testData <- df.shuffled[testIndexes, ]
   trainData <- df.shuffled[-testIndexes, ]
   
-  #k-fold cv to evaluate models
+  # cross-validation
   for (j in 1:degree){
     fit.train = lm(Final.Price ~ poly(Bidders,j), data=trainData)
     fit.test = predict(fit.train, newdata=testData)
     mse[i,j] = mean((fit.test-testData$Final.Price)^2) 
   }
 }
-
-#find MSE for each degree 
-colMeans(mse)
 
 # fit best model
 best = lm(Final.Price ~ poly(Bidders,2, raw=T), data=df)
@@ -153,3 +135,17 @@ ggplot(df, aes(x=Bidders, y=Final.Price)) +
   stat_smooth(method='lm', formula = y ~ poly(x,2), size = 1) + 
   xlab('SD of price difference between consecutive bids') +
   ylab('Final Price')
+
+
+################################
+# Principal Component Analysis #        Python Implementation is more complete
+############################## #
+
+dff = df[1:7]
+
+#pca = PCA(df, scale.unit=TRUE, ncp=2, graph=T)
+pca=PCA(dff, quanti.sup=1)
+plot(pca,habillage= "Final.Price",select="cos2 0.6")
+
+
+
